526bf0614543686cc1c5de2906cd2d6d00898218
mleila <mohamed.laila@mail.mcgill.ca>
2020-07-13 17:52:12 -0400

    [NEW] add BFS and fix DFS

--- algos/graph.py
+++ algos/graph.py
@@ -1,3 +1,7 @@
+from collections import deque
+
+from numpy import inf
+
 from ds import Node, LinkedList
 
 
@@ -5,6 +9,7 @@ class AdjacencyList:
 
     def __init__(self):
         self.array = []
+        self.inf = inf
 
     def from_matrix(self, matrix: list):
         """Convert adjacency matrix to adjacency list."""
@@ -18,19 +23,57 @@ class AdjacencyList:
                 if vertex:
                     ll.insert_node(Node(val=vert_indx))
 
+    def bfs(self):
+        """Run BFS."""
+        # create Queue
+        q = deque()
+
+        # book keeping
+        visited = [False]*len(self.array)
+        parent = [None]*len(self.array)
+        distance = [self.inf]*len(self.array)
+        traversal = []
+
+        # initialize source
+        q.appendleft(0)
+        distance[0] = 0
+        parent[0] = 0
+        visited[0] = True
+
+        while len(q):
+            curr = q.pop()
+            traversal.append(curr)
+            distance[curr] = distance[parent[curr]] + 1
+            children = self.array[curr].tolist()
+            for child in children:
+                if visited[child]:
+                    continue
+                visited[child] = True
+                parent[child] = curr
+                q.appendleft(child)
+        return traversal
+
     def dfs(self):
         visited = [False]*len(self.array)
         stack = [0]
         traversal = []
+        visited[0] = True
         while len(stack):
             curr = stack.pop()
-            visited[curr] = True
             traversal.append(curr)
             children = self.array[curr].tolist()
-            if children == []:
-                continue
             for child in children:
                 if visited[child]:
                     continue
+                visited[child] = True
                 stack.append(child)
         return traversal
+
+    def __str__(self):
+        s = ''
+        for indx, ll in enumerate(self.array):
+            s += f'Node({indx})->{ll.tolist()}\n'
+        return s
+
+    def __repr__(self):
+        return str(self)
