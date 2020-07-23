990f6ced75dcfc2889eb5e53865d3a1ae5c70528
mleila <mohamed.laila@mail.mcgill.ca>
2020-07-23 19:27:46 -0400

    [NEW] start tarjan

--- algos/graph.py
+++ algos/graph.py
@@ -5,75 +5,42 @@ from numpy import inf
 from ds import Node, LinkedList
 
 
-class AdjacencyList:
+def tarjan(adjlist):
+    """
+    Tarjan algorithm for finding strongly connected components
+    """
+    seen = set()
+    counter = 0
 
-    def __init__(self):
-        self.array = []
-        self.inf = inf
+    # init
+    for node in adjlist:
+        node.label = None
+        node.low_link = None
 
-    def from_matrix(self, matrix: list):
-        """Convert adjacency matrix to adjacency list."""
-        # init the array
-        self.array = []
-        rows = len(matrix)
-        self.array = [LinkedList() for row in range(rows)]
-        for idx, row in enumerate(matrix):
-            ll = self.array[idx]
-            for vert_indx, vertex in enumerate(row):
-                if vertex:
-                    ll.insert_node(Node(val=vert_indx))
+    for node in adjlist:
+        # full explore
+        if node in seen:
+            continue
 
-    def bfs(self):
-        """Run BFS."""
-        # create Queue
-        q = deque()
+        # run dfs with bookkeeping
+        stack = []
+        node.label = counter
+        counter += 1
+        stack.append(node)
 
-        # book keeping
-        visited = [False]*len(self.array)
-        parent = [None]*len(self.array)
-        distance = [self.inf]*len(self.array)
-        traversal = []
-
-        # initialize source
-        q.appendleft(0)
-        distance[0] = 0
-        parent[0] = 0
-        visited[0] = True
-
-        while len(q):
-            curr = q.pop()
-            traversal.append(curr)
-            distance[curr] = distance[parent[curr]] + 1
-            children = self.array[curr].tolist()
-            for child in children:
-                if visited[child]:
-                    continue
-                visited[child] = True
-                parent[child] = curr
-                q.appendleft(child)
-        return traversal
-
-    def dfs(self):
-        visited = [False]*len(self.array)
-        stack = [0]
-        traversal = []
-        visited[0] = True
+        node.low_link = node.label
         while len(stack):
             curr = stack.pop()
-            traversal.append(curr)
-            children = self.array[curr].tolist()
+            seen.add(curr)
+            children = adjlist.array[curr].tolist()
             for child in children:
-                if visited[child]:
+                if child in seen:
+                    curr.low_link = min(curr.low_link, child.low_link)
                     continue
-                visited[child] = True
+                child.label = counter
+                counter += 1
+                child.low_link = child.label
                 stack.append(child)
-        return traversal
 
-    def __str__(self):
-        s = ''
-        for indx, ll in enumerate(self.array):
-            s += f'Node({indx})->{ll.tolist()}\n'
-        return s
-
-    def __repr__(self):
-        return str(self)
+        for n in adjlist:
+            print(n.label, n.low_link)
